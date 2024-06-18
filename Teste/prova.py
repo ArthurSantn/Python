5e58ff7a8a9a65e5ac32fa60ed3eecdbdc87ebd5
Arthur Pereira Santana <arthurklotes1@gmail.com>
2024-06-18 19:52:41 -0300

    Create prova.py

---
+++ Teste/prova.py
@@ -0,0 +1,17 @@
+""""
+n = 1
+
+while n < 10:
+  n += 1
+  print (n)
+  
+"""
+while True:
+    print('1 - Cadastrar usuário\n2 - Logar usuário\n0 - Sair')
+    escolha = int(input('Selecione uma opção ==>'))
+    if escolha == 1:
+      pass
+    elif escolha == 2:
+       pass
+    else:
+       break
